1a9fafb142e3ff9d88151be27b437ce73beb4086
Tyler Johnson <tjohnson@control4.com>
2017-04-05 21:27:12 -0600

    added a whole lot of stuf

--- policy.py
+++ policy.py
@@ -49,6 +49,98 @@ identity = (
     "preferred brand of vehicle"
 )
 
+party = (
+    "Independant",
+    "Libertarian",
+    "Republican",
+    "Democrat",
+    "Facist",
+    "Communist",
+    "Socialist",
+    "statist",
+    "Progressive",
+    "Constitutionalist",
+    "Nazi"
+)
+
+profession = (
+    "school teacher",
+    "programmer",
+    "rocket scientist",
+    "potato farmer",
+    "mechanic",
+    "medical doctor",
+    "congress representative",
+    "fast food server",
+    "Professors",
+    "Teachers",
+    "Cultural[edit]",
+    "Clergy",
+    "Philosopher",
+    "Medical[edit]",
+    "Anesthesiologist",
+    "Audiologist",
+    "Chiropractor",
+    "Dentist",
+    "Dietitian",
+    "Nurses",
+    "Occupational therapist",
+    "Pharmacist",
+    "Operating Department Practitioner",
+    "Optometrist",
+    "Pharmacist",
+    "Physical therapist",
+    "Physician",
+    "Podiatrist",
+    "Psychologist",
+    "Radiographer",
+    "Radiotherapist",
+    "Speech-language pathologist",
+    "Surgeon",
+    "Veterinarian",
+    "Accountant",
+    "Actuarie",
+    "Architect",
+    "Engineer",
+    "Linguistic",
+    "Translator",
+    "Interpreter",
+    "Surveyor",
+    "Urban Planner",
+    "Air traffic controller",
+    "Aircraft pilot",
+    "Sea captain",
+    "Lawyers",
+    "Social Workers",
+    "Health inspector",
+    "Park ranger",
+    "Police officer",
+    "Military officer",
+    "Scientist",
+    "Astronomer",
+    "Biologist",
+    "Botanist",
+    "Ecologist",
+    "Geneticist",
+    "Immunologist",
+    "Paleontologist",
+    "Pharmacologist",
+    "Virologist",
+    "Zoologist",
+    "Chemist",
+    "Geologist",
+    "Meteorologist",
+    "Neuroscientist",
+    "Oceanographer",
+    "Physicist"
+)
+
+adjective = (
+    "femminist",
+    "chovenist",
+    "polygymist"
+)
+
 objects = (
     "rocket launchers",
     "coat hangers",
@@ -72,29 +164,40 @@ products = (
     "tamales",
     "firearms",
     "property",
-    "gasoline"
+    "gasoline",
+    "rainwater"
+)
+
+without = (
+    "paying a tax",
+    "a permit",
+    "government approval",
+    "the government's permission",
+    "consent",
+    "a city permit"
 )
 
 action = (
-    "to buy " + choice(products) + " without paying sales tax.",
-    "to continue living on property they have already purchased without a continual property tax.",
-    "to collect rainwater.",
-    "to fire people based only on their " + choice(identity) + ".",
+    "to buy " + choice(products) + " without " + choice(without) + ".",
+    "to continue living on property they have already purchased without " + choice(without) + ".",
+    "to collect " + choice(products) + ".",
+    "to fire " + choice(profession) + "s based only on their " + choice(identity) + ".",
+    "to fire " + choice(profession) + "s soley because they may be " + choice(adjective + party) + ".",
     "to hire people based only on their " + choice(identity) + ".",
     "to own and operate " + choice(objects) + ".",
     "to perform open heart surgery with " + choice(objects) + ".",
     "to defend their marijuana fields with " + choice(objects) + ".",
-    "to sell " + choice(products) + " without a permit.",
+    "to sell " + choice(products) + " without " + choice(without) + ".",
     "to take down trees with " + choice(objects) + " on their own property.",
-    "to paint their house without a city permit.",
-    "to get married without the government's permission.",
-    "to build " + choice(objects) + " in their backyard without a permit."
+    "to paint their house without " + choice(without) + ".",
+    "to get married without " + choice(without) + ".",
+    "to build " + choice(objects) + " in their backyard without " + choice(without) + "."
 )
 
 
 
 def get_policy():
-    return choice(libertarian) + " " +  choice(subject) +  " " + choice(action)
+    return choice(libertarian) + " " +  choice((choice(subject),choice(profession) + "s")) +  " " + choice(action)
 
 
 if __name__ == "__main__":
